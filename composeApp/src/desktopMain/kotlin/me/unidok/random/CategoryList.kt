e8a6371d6fcbc3df8f9672e0767f02cf1217f819
unidok <operatorgdm@gmail.com>
2025-05-12 19:00:51 +0300

    Initial commit

---
+++ composeApp/src/desktopMain/kotlin/me/unidok/random/CategoryList.kt
@@ -0,0 +1,137 @@
+package me.unidok.random
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+enum class Category(
+    val displayName: String
+) {
+    NUMBER("Число") {
+        @Composable
+        override fun screen() = RandomNumberGenerator()
+    },
+
+    UUID("UUID") {
+        @Composable
+        override fun screen() = RandomUUIDGenerator()
+    };
+
+    @Composable
+    abstract fun screen()
+}
+
+@Composable
+fun CategorySelectionScreen() {
+    var selectedCategory by remember { mutableStateOf(Category.NUMBER) }
+
+    Row(modifier = Modifier.fillMaxSize()) {
+        CategoryList(
+            selectedCategory = selectedCategory,
+            onCategorySelected = { selectedCategory = it },
+            modifier = Modifier
+                .width(200.dp)
+                .fillMaxHeight()
+                .background(MaterialTheme.colors.surface)
+        )
+
+        Divider(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(2.dp),
+            color = MaterialTheme.colors.primaryVariant
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            selectedCategory.screen()
+        }
+    }
+}
+
+@Composable
+fun CategoryList(
+    selectedCategory: Category,
+    onCategorySelected: (Category) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(0.dp),
+        verticalArrangement = Arrangement.spacedBy(0.dp)
+    ) {
+        items(Category.entries) { category ->
+            CategoryItem(
+                category = category,
+                isSelected = category == selectedCategory,
+                onCategorySelected = onCategorySelected
+            )
+
+            Divider(
+                thickness = 1.dp,
+                color = MaterialTheme.colors.primaryVariant
+            )
+        }
+    }
+}
+
+@Composable
+fun CategoryItem(
+    category: Category,
+    isSelected: Boolean,
+    onCategorySelected: (Category) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    // Цвета для выбранной и невыбранной категорий
+    val backgroundColor = if (isSelected) {
+        MaterialTheme.colors.primary
+    } else {
+        Color.Transparent
+    }
+
+    val textColor = if (isSelected) {
+        MaterialTheme.colors.onPrimary // Белый текст на primary-цвете
+    } else {
+        MaterialTheme.colors.onSurface // Стандартный цвет текста
+    }
+
+    TextButton(
+        onClick = { onCategorySelected(category) },
+        modifier = modifier
+            .fillMaxWidth()
+            .height(48.dp),
+        colors = ButtonDefaults.textButtonColors(
+            backgroundColor = backgroundColor
+        ),
+        contentPadding = PaddingValues(0.dp),
+        shape = RectangleShape
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp), // Горизонтальные отступы только для текста
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Text(
+                text = category.displayName,
+                color = textColor,
+                style = MaterialTheme.typography.body1.copy(
+                    fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal
+                )
+            )
+        }
+    }
+}
---
+++ composeApp/src/desktopMain/kotlin/me/unidok/random/Main.kt
@@ -0,0 +1,66 @@
+package me.unidok.random
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Рандомайзер",
+        icon = rememberVectorPainter(Icons.Default.Menu)
+    ) {
+        MaterialTheme {
+            CategorySelectionScreen()
+        }
+    }
+}
+
+@Composable
+fun ResultField(
+    result: String?,
+    style: TextStyle = LocalTextStyle.current,
+    onClear: () -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column {
+            SelectionContainer {
+                Text(
+                    text = result ?: "",
+                    style = style
+                )
+            }
+        }
+
+        Spacer(Modifier.width(20.dp))
+
+        Column {
+            if (result != null) {
+                IconButton(
+                    onClick = onClear,
+                    modifier = Modifier
+                        .size(30.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = null,
+                        tint = MaterialTheme.colors.primary
+                    )
+                }
+            }
+        }
+    }
+}
